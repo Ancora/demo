e03f9abc053ec631b4821c9c777f0a5daf1a870d
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-22 15:05:55 -0300

    56-Utilizando um Package/Plugin local.

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/demo.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -47,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = Calculator().addOne(_counter);
     });
   }
 
